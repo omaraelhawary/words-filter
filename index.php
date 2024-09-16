86b8bb1406f4613c3af1c1092f2fb16eaf1e8689
Omar ElHawary <omaraelhawary@gmail.com>
2024-09-17 00:15:31 +0300

    Add submenu for Menu

--- index.php
+++ index.php
@@ -14,12 +14,22 @@ class WordsFilter {
         add_action('admin_menu', array($this, 'ourMenu'));
     }
     function ourMenu(){
-        add_menu_page('ًWords Filters', 'Words Filters', 'manage_options', 'words-filter', array($this, 'wordFilterPage'), 'dashicons-text', 110);
+        add_menu_page('ًWords Filter', 'Words Filter', 'manage_options', 'words-filter', array($this, 'wordsFilterPage'), 'dashicons-text', 110);
+
+        add_submenu_page('words-filter', 'Words to Filter', 'Words List', 'manage_options', 'words-filter', array($this, 'wordsFilterPage'));
+
+        add_submenu_page('words-filter', 'Word Filter Options', 'Options', 'manage_options', 'words-filter-option', array($this, 'Wordsfilteroptions'));
     }
 
-    function wordFilterPage(){ ?>
+    function wordsFilterPage(){ ?>
 
 <h1>Word Filter</h1>
+
+<?php }
+
+    function Wordsfilteroptions(){ ?>
+
+<h1>Options page</h1>
 
 <?php }
 
