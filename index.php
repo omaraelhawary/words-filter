1de92ebd4bf8f32631510d8c0d6f5a2a4d4fffc5
Omar ElHawary <omaraelhawary@gmail.com>
2024-09-16 22:14:36 +0300

    Init Words Filter plugin

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+/*
+    Plugin Name: Words Filter
+    Description: Replace a list of words.
+    Version: 1.0
+    Author: Omar ElHawary
+    Author URI: https://www.linkedin.com/in/omaraelhawary/
+*/
+
+if(!defined('ABSPATH')) exit;
+
+class WordsFilter {
+    function __construct(){
+
+    }
+}
+
+$wordsFilter = new WordsFilter();
