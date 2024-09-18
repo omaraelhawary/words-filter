939e12275888a1dca8f051972af8d360cd4607ec
Omar ElHawary <omaraelhawary@gmail.com>
2024-09-18 17:09:35 +0300

    Enqueue custom CSS file for plugin

--- index.php
+++ index.php
@@ -15,12 +15,19 @@ class WordsFilter {
     }
     function ourMenu(){
 
-        add_menu_page('ًWords Filter', 'Words Filter', 'manage_options', 'words-filter', array($this, 'wordsFilterPage'), plugin_dir_url(__FILE__) . 'icon.svg', 110);
+        $mainPageHook = add_menu_page('ًWords Filter', 'Words Filter', 'manage_options', 'words-filter', array($this, 'wordsFilterPage'), plugin_dir_url(__FILE__) . 'icon.svg', 110);
 
         add_submenu_page('words-filter', 'Words to Filter', 'Words List', 'manage_options', 'words-filter', array($this, 'wordsFilterPage'));
 
         add_submenu_page('words-filter', 'Word Filter Options', 'Options', 'manage_options', 'words-filter-option', array($this, 'Wordsfilteroptions'));
+
+        add_action("load-{$mainPageHook}", array($this, 'mainPageAssets'));
     }
+
+    function mainPageAssets(){
+        wp_enqueue_style('words-filter-styles', plugin_dir_url(__FILE__) . 'style.css');
+    }
+
 
     function wordsFilterPage(){ ?>
 
@@ -30,7 +37,7 @@ class WordsFilter {
                 <label for="filterWords">
                     <p> Enter a <strong>comma-sepratated</strong> list of words to filter from your site content</p>
                 </label>
-                <div class="wprd-filter__flex-container">
+                <div class="word-filter__flex-container">
                     <textarea name="filterWords" id="filterWords" placeholder="bad, mean, awful"
                         class="wprd-filter__textarea"></textarea>
                 </div>
